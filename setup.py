416ac8e8eb8d6ddf388f91c4c7ae393504047c76
Chloe M Mawer <chloe.m.mawer@gmail.com>
2020-11-25 15:39:58 -0500

    Add geo data to setup

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author="Chloe Mawer",
     author_email="chloe.m.mawer@gmail.com",
     version="0.1.0",
-    packages=["vishelper"],
-    # package_dir={'': 'vishelper'}
+    packages=find_packages(),
+    package_data={'vishelper': ['geo/*json', 'geo/*csv']}
 )
